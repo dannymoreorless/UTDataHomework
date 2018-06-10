f568aa232b4beb1575b7b4d68ce4a67467771a11
Daniel Morales <36523778+dannymoreorless@users.noreply.github.com>
2018-06-10 15:08:14 -0500

    Add files via upload

---
+++ HW 7 Social Analytics/config.py
@@ -0,0 +1,6 @@
+#config.py for tweepy
+
+consumer_key = "dhNebwJAmeINC4bfWD94Iqwmm"                       
+consumer_secret = "DcQdXPEesm9qu9gtpUVDICFoDMINTtDteZ7iOCVg36gOsDNOMO" 
+access_token = "375361002-RxxlyNltg1kqX9AmPTRo1NKhY20M7zgi3XImHbqh" 
+access_token_secret ="YvZEOJoWGKWvDkNiv1fFG1elEHa8SaCLyzrBI8jxaTBtN"
